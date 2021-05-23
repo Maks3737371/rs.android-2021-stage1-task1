8f93d45a8d86ff145773a7b235695a92971696c5
maksim_y <maksimyarmolich@mail.ru>
2021-05-23 19:16:35 +0300

    add: сделал первое задание

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,18 @@
 package subtask1
 
-class HappyArray {
-
-    // TODO: Complete the following function
-    fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+class HappyArray
+{
+    fun convertToHappy(sadArray: IntArray): IntArray
+    {
+        val happyArray = sadArray.toMutableList()
+        var i = 0
+        while (i<happyArray.size-1) {
+            if (i>0 && happyArray[i]>happyArray[i-1]+happyArray[i+1]){
+                happyArray.removeAt(i)
+                i--
+            } else i++
+        }
+        return happyArray.toIntArray()
+        //throw NotImplementedError("Not implemented")
     }
 }
