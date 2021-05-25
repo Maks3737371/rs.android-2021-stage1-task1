a740681f96a8553ff218dc9227ba380227a769f3
maksim_y <maksimyarmolich@mail.ru>
2021-05-25 20:57:49 +0300

    add: доделал 3 задание

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,7 +4,34 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        
-        //throw NotImplementedError("Not implemented")
+        val tempArray :MutableList<String> = mutableListOf()
+        for (i in inputString.indices)
+        {
+            when (inputString[i])
+            {
+                '(' -> tempArray.add(findSubstring(inputString, '(', ')', i))
+                '[' -> tempArray.add(findSubstring(inputString, '[', ']', i))
+                '<' -> tempArray.add(findSubstring(inputString, '<', '>', i))
+            }
+        }
+        return tempArray.toTypedArray()
+    }
+
+    private fun findSubstring(string: String, openBracket: Char, closeBracket: Char, index: Int): String{
+        var subString: String = ""
+        var subStrIndex=index+1
+        var counter = 0
+
+        while(string[subStrIndex]!=closeBracket || counter>0)
+        {
+            if (string[subStrIndex]==openBracket)
+                counter++
+            else
+                if (string[subStrIndex]==closeBracket)
+                counter--
+            subString += string[subStrIndex]
+            subStrIndex++
+        }
+        return subString
     }
 }
